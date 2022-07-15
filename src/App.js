939188cab8019211d4ba70bd5f51f5b79268f7bd
MRJAHID <Mrjahid350@gmail.com>
2022-07-15 17:44:36 +0600

    Project Finish

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Header from './components/Header';
 import './App.css';
 import OptionsTab from 'components/OptionsTab';
+import LocationCards from 'components/LocationCards';
+import { Container } from '@mui/material';
+import Footer from 'components/Footer';
+import FooterMenu from 'components/FooterMenu';
+import { displayOnDesktop } from 'themes/commonStyles';
 
 function App() {
     return (
@@ -17,6 +22,24 @@ function App() {
                 <Box>
                     <Header />
                     <OptionsTab />
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        flexGrow: 1,
+                        height: 100,
+                        overflowY: 'scroll',
+                    }}>
+                    <Container maxWidth="xl" sx={{ mb: 3 }}>
+                        <LocationCards />
+                    </Container>
+                </Box>
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <FooterMenu />
+                </Box>
+                <Box sx={displayOnDesktop}>
+                    <Footer />
                 </Box>
             </Box>
         </>
